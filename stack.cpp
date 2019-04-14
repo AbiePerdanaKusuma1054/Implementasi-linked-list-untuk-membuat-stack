da1bc3f72ffb1200f1f7c3febdf68bdd2392338f
AbiePerdanaKusuma1054 <44666830+AbiePerdanaKusuma1054@users.noreply.github.com>
2019-04-14 15:56:43 +0700

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -27,3 +27,52 @@ void listdata(){
 		bantu->next=baru;
 	}	
 }
+void push(){
+	element *baru;
+	baru=new element;
+	cout<<"Masukkan Data :";
+	cin>>baru->data;
+	baru->next=NULL;
+	if(isEmpty()){
+		top=baru;
+		top->next=NULL;
+	}
+	else{
+		baru->next=top;
+		top=baru;
+	}
+}
+void pop(){
+	int q;
+	element *baru;
+	if(top==NULL){
+		cout<<"Stack masih kosong"<<endl;
+	}
+	else{
+		baru=top;
+		top=top->next;
+		delete baru;
+	}
+}
+void topdata(){
+	if(isEmpty()){
+		cout<<"Stack masih kosong"<<endl;
+	}
+	else{
+		cout<<"Data Top : "<<top->data<<endl;
+	}
+}
+void printstack (){
+	element *bantu;
+	if(isEmpty()){
+		cout<<"Stack Kosong"<<endl;
+	}
+	else{
+		bantu=top;
+		while(bantu!=NULL){
+			cout<<"|\t"<<bantu->data<<"\t|"<<endl;
+			bantu=bantu->next;
+		}
+		cout<<endl;
+	}
+}
